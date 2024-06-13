dff8efa973b5d8256abe72b5d165ebba779cdc6c
Edvomberto Honorato <e.honorato@uol.com.br>
2024-06-13 17:50:25 -0300

    .....

--- src/pages/LoginLinkedin.jsx
+++ src/pages/LoginLinkedin.jsx
@@ -7,7 +7,7 @@ const LoginLinkedin = () => {
   useEffect(() => {
     if (!redirected) {
       setRedirected(true);
-      window.location.href = 'https://ws-booster-social-5040b10dd814.herokuapp.com/loginLinkedin';
+      window.location.href = '/loginLinkedin';
     }
   }, [redirected]);
 
