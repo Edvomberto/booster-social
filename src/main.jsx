5d8fd9430521158c01661ea15bc2e678b0de8899
Edvomberto Honorato <e.honorato@uol.com.br>
2024-06-13 13:51:31 -0300

    configurações de de deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@coreui/coreui/dist/css/coreui.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/booster-social"
+  base: "/booster-social/",
+  server: {
+    historyApiFallback: true, // Certifique-se de que o Vite lide com o roteamento de SPA
+  },
+  publicDir: 'public' // Adicione esta linha para garantir que os arquivos públicos sejam servidos
 })
