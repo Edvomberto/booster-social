512914daf670f798cb5d1edb6c194aac66e848f9
Edvomberto Honorato <e.honorato@uol.com.br>
2024-06-13 17:50:03 -0300

    ....

--- src/pages/LoginLinkedin.jsx
+++ src/pages/LoginLinkedin.jsx
@@ -11,7 +11,7 @@ const LoginLinkedin = () => {
     }
   }, [redirected]);
 
-  return <div>Redirecionando para o LinkedIn...</div>;
+  return <div>Autenticando no LinkedIn...</div>;
 };
 
 export default LoginLinkedin;
